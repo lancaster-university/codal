fcb21268eb331f2a12ff7c19111868ed060cbe14
Devine <j.devine@lancaster.ac.uk>
2016-11-01 11:36:02 +0000

    WIP: simplified context switch

--- inc/core/DeviceFiber.h
+++ inc/core/DeviceFiber.h
@@ -58,59 +58,8 @@ DEALINGS IN THE SOFTWARE.
   */
 struct AVR_TCB
 {
-    uint8_t R[0];
-    uint8_t R0;
-    uint8_t R1;
-    uint8_t R2;
-    uint8_t R3;
-    uint8_t R4;
-    uint8_t R5;
-    uint8_t R6;
-    uint8_t R7;
-    uint8_t R8;
-    uint8_t R9;
-    uint8_t R10;
-    uint8_t R11;
-    uint8_t R12;
-    uint8_t R13;
-    uint8_t R14;
-    uint8_t R15;
-    uint8_t R16;
-    uint8_t R17;
-    uint8_t R18;
-    uint8_t R19;
-    uint8_t R20;
-    uint8_t R21;
-    uint8_t R22;
-    uint8_t R23;
-    uint8_t R24;
-    uint8_t R25;
-
-    // indirect x
-    uint16_t X[0];
-    uint8_t R26;
-    uint8_t R27;
-
-    // indirect y
-    uint16_t Y[0];
-    uint8_t R28;
-    uint8_t R29;
-
-    // indirect z
-    uint16_t Z[0];
-    uint8_t R30;
-    uint8_t R31;
-
-    // status reg
-    uint8_t SR;
-
-    // stack pointer
-    uint16_t SPREG[0];
     uint8_t SPLO;
     uint8_t SPHI;
-
-    // stack base
-    uint16_t stack_base;
 
     uint16_t lr;
 };
